913c57445bde7918cdd7dfa0d7d18ab8610409de
briggsb9 <ben_briggs@outlook.com>
2024-09-10 16:10:11 +0100

    debian odbc

--- scripts/prepdata.sh
+++ scripts/prepdata.sh
@@ -12,22 +12,15 @@ $(azd env get-values)
 EOF
 
 echo 'Installing ODBC driver'
-if ! [[ "18.04 20.04 22.04 23.04 24.04" == *"$(lsb_release -rs)"* ]];
-then
-    echo "Ubuntu $(lsb_release -rs) is not currently supported.";
-    exit;
-fi
+# Debian 12
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
 
-# Add the signature to trust the Microsoft repo
-# For Ubuntu versions < 24.04 
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
-# For Ubuntu versions >= 24.04
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+#Download appropriate package for the OS version
+#Choose only ONE of the following, corresponding to your OS version
 
-# Add repo to apt sources
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+#Debian 12
+curl https://packages.microsoft.com/config/debian/12/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
 
-# Install the driver
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
 # optional: for bcp and sqlcmd
